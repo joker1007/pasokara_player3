53f77b2f3be8a77b8a095c50eb5c0c433bb0c582
joker1007 <kakyoin.hierophant@gmail.com>
2011-06-03 21:52:34 +0900

    First commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PasokaraPlayerRails3::Application.config.secret_token = '707a5dcb77772e6bbd3773ca3b7231206766ba381889da3a14b1531eb8ce26095df955ee7b173bd7dad7731519883b0b2057efd2b3ea1ba457991074ff781e87'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+PasokaraPlayerRails3::Application.config.session_store :cookie_store, :key => '_pasokara_player_rails3_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# PasokaraPlayerRails3::Application.config.session_store :active_record_store
