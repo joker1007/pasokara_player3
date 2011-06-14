329c0fe0cf3a82211862061fa42f068eb30372d7
joker1007 <kakyoin.hierophant@gmail.com>
2011-06-15 00:52:56 +0900

    不要になったhelperメソッドを削除

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -39,33 +39,6 @@ module ApplicationHelper
     info_box(pasokara)
   end
 
-  def info_box(entity)
-    %Q{
-      <div id="info-box-#{entity.id}" class="info_box">
-        <h3>タグ</h3>
-        #{tag_list(entity)}
-        <hr />
-        <h3>動画情報</h3>
-        #{info_list(entity)}
-      </div>
-    }
-  end
-
-  def info_list(entity)
-    %Q{
-      <div id="info-list-#{entity.id}" class="info_list">
-        <div class="thumb clearfix">#{image_tag(thumb_pasokara_path(entity.id), :size => "160x120", :class => "thumb")}</div>
-        <div class="nico_info clearfix">
-          <span class="info_key">ニコニコID:</span><span class="info_value">#{link_to(entity.nico_name, entity.nico_url) if entity.nico_name}</span><br />
-          <span class="info_key">投稿日:</span><span class="info_value">#{h entity.nico_post_str}</span><br />
-          <span class="info_key">再生数:</span><span class="info_value">#{number_with_delimiter(entity.nico_view_counter)}</span><br />
-          <span class="info_key">コメント数:</span><span class="info_value">#{number_with_delimiter(entity.nico_comment_num)}</span><br />
-          <span class="info_key">マイリスト数:</span><span class="info_value">#{number_with_delimiter(entity.nico_mylist_counter)}</span><br />
-        </div>
-      </div>
-    }
-  end
-
   def tag_list(entity)
     content_tag("div", {:id => "tag-list-#{entity.id}", :class => "tag_list"}) do
       entity.tag_list.inject("") do |str, p_tag|
@@ -76,26 +49,6 @@ module ApplicationHelper
       end
       #link_to_remote("[編集]", :url => tag_form_open_path(:id => entity), :html => {:href => tag_form_open_path(:id => entity), :class => "tag_edit_link"})
     end
-  end
-
-  def navi_bar
-    str = %Q{
-      <ul id="menu">
-        <li>#{link_to("ホーム", {:controller => 'dir', :action => 'index'}, :id => "home")}</li>
-        <li>#{link_to("予約確認", {:controller => 'queue', :action => 'list'}, :id => "queue")}</li>
-        <li>#{link_to("りれき", {:controller => 'sing_log', :action => 'list'}, :id => "history")}</li>
-    }
-    #if current_user
-      #str += %Q{
-        #<li>#{link_to("お気に入り", {:controller => 'favorite', :action => 'list'}, :id => "favorite")}</li>
-        #<li>#{link_to("設定変更", edit_user_path(current_user.id), :id => "user_edit")}</li>
-      #}
-    #end
-    str += %Q{
-        <li>#{link_to("使い方", {:controller => 'help', :action => 'usage'}, :id => "usage")}</li>
-      </ul>
-    }
-    str
   end
 
 end
