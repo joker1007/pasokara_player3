11214026ab7b2557e68a9151ae5f9ff9127233ee
joker1007 <kakyoin.hierophant@gmail.com>
2011-06-21 22:59:17 +0900

    sporkのためにspec_helperを修正

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,22 +1,4 @@
-require 'rubygems'
 require 'spork'
-
-Spork.prefork do
-  # Loading more in this block will cause your tests to run faster. However,
-  # if you change any configuration or code from libraries loaded here, you'll
-  # need to restart spork for it take effect.
-  ActiveSupport::Dependencies.clear
-
-end
-
-Spork.each_run do
-  # This code will be run each time you run your specs.
-
-end
-
-if Spork.using_spork?
-  ActiveSupport::Dependencies.clear
-end
 
 # --- Instructions ---
 # Sort the contents of this file into a Spork.prefork and a Spork.each_run
@@ -48,46 +30,72 @@ end
 # free to delete them.
 
 
+Spork.prefork do
+  ENV["RAILS_ENV"] ||= 'test'
+  # Loading more in this block will cause your tests to run faster. However,
+  # if you change any configuration or code from libraries loaded here, you'll
+  # need to restart spork for it take effect.
+  require "rails/mongoid"
+  Spork.trap_class_method(Rails::Mongoid, :load_models)
 
+  require "rails/application"
+  Spork.trap_method(Rails::Application, :reload_routes!)
 
-# This file is copied to spec/ when you run 'rails generate rspec:install'
-ENV["RAILS_ENV"] ||= 'test'
-require File.expand_path("../../config/environment", __FILE__)
-require 'rspec/rails'
+  # This file is copied to spec/ when you run 'rails generate rspec:install'
+  require File.expand_path("../../config/environment", __FILE__)
+  require 'rspec/rails'
 
-# Requires supporting ruby files with custom matchers and macros, etc,
-# in spec/support/ and its subdirectories.
-Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
+  # Requires supporting ruby files with custom matchers and macros, etc,
+  # in spec/support/ and its subdirectories.
+  Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
 
-RSpec.configure do |config|
-  config.include Mongoid::Matchers
-  # == Mock Framework
-  #
-  # If you prefer to use mocha, flexmock or RR, uncomment the appropriate line:
-  #
-  # config.mock_with :mocha
-  # config.mock_with :flexmock
-  # config.mock_with :rr
-  config.mock_with :rspec
+  RSpec.configure do |config|
+    config.include Mongoid::Matchers
+    # == Mock Framework
+    #
+    # If you prefer to use mocha, flexmock or RR, uncomment the appropriate line:
+    #
+    # config.mock_with :mocha
+    # config.mock_with :flexmock
+    # config.mock_with :rr
+    config.mock_with :rspec
 
-  # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
-  #config.fixture_path = "#{::Rails.root}/spec/fixtures"
+    # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
+    #config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
-  # If you're not using ActiveRecord, or you'd prefer not to run each of your
-  # examples within a transaction, remove the following line or assign false
-  # instead of true.
-  #config.use_transactional_fixtures = true
+    # If you're not using ActiveRecord, or you'd prefer not to run each of your
+    # examples within a transaction, remove the following line or assign false
+    # instead of true.
+    #config.use_transactional_fixtures = true
 
-  config.before(:suite) do
-    DatabaseCleaner.strategy = :truncation
-    DatabaseCleaner.clean_with(:truncation)
+    config.before(:suite) do
+      DatabaseCleaner.strategy = :truncation
+      DatabaseCleaner.clean_with(:truncation)
+    end
+
+    config.before(:each) do
+      DatabaseCleaner.start
+    end
+
+    config.after(:each) do
+      DatabaseCleaner.clean
+    end
   end
 
-  config.before(:each) do
-    DatabaseCleaner.start
-  end
+  ActiveSupport::Dependencies.clear
 
-  config.after(:each) do
-    DatabaseCleaner.clean
-  end
 end
+
+Spork.each_run do
+  # This code will be run each time you run your specs.
+  Factory.factories.clear
+  Dir[Rails.root.join("spec/factories/**/*.rb")].each{|f| load f}
+end
+
+if Spork.using_spork?
+  ActiveSupport::Dependencies.clear
+end
+
+
+
+
