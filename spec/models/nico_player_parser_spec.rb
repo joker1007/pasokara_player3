7277fd65e77dfa05d7e4b2a89f9abc1bc611fb48
joker1007 <kakyoin.hierophant@gmail.com>
2011-10-16 23:49:08 +0900

    refactoring nico_player_parser_spec

--- spec/models/nico_player_parser_spec.rb
+++ spec/models/nico_player_parser_spec.rb
@@ -3,31 +3,40 @@ require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe NicoParser::NicoPlayerParser do
 
-  it "NicoPlayer形式のinfoテキストからタグをパースして、配列で返すこと" do
-    tags = NicoParser::NicoPlayerParser.new.parse_tag File.expand_path(File.dirname(__FILE__) + '/../romio_nico_player_info.txt')
-    tags.should == [
-      "歌ってみた",
-      "ニコニコカラオケＤＢ",
-      "ボカロカラオケDB",
-      "よっぺい",
-      "ロミオとシンデレラ",
-      "黙ると死ぬ男",
-      "原曲ブレイカー",
-      "むしろこっちが原曲",
-      "なぜ作ったし",
-      "作業用BGM",
-    ]
+  before do
+    @tags = NicoParser::NicoPlayerParser.new.parse_tag File.expand_path(File.dirname(__FILE__) + '/../romio_nico_player_info.txt')
+    @infos = NicoParser::NicoPlayerParser.new.parse_info File.expand_path(File.dirname(__FILE__) + '/../romio_nico_player_info.txt')
   end
 
-  it "NicoPlayer形式のinfoテキストから動画情報をパースして、Hashで返すこと" do
-    infos = NicoParser::NicoPlayerParser.new.parse_info File.expand_path(File.dirname(__FILE__) + '/../romio_nico_player_info.txt')
-    infos.should == {
-      :nico_name => "sm6875851",
-      :nico_post => "2009/04/27 23:59:25",
-      :nico_view_counter => 6431,
-      :nico_comment_num => 340,
-      :nico_mylist_counter => 229,
-      :nico_description => "投稿者のコメント欄",
-    }
+  describe "タグのパース" do
+    subject {@tags}
+    it "NicoPlayer形式のinfoテキストからタグをパースして、配列で返すこと" do
+      subject.should == [
+        "歌ってみた",
+        "ニコニコカラオケＤＢ",
+        "ボカロカラオケDB",
+        "よっぺい",
+        "ロミオとシンデレラ",
+        "黙ると死ぬ男",
+        "原曲ブレイカー",
+        "むしろこっちが原曲",
+        "なぜ作ったし",
+        "作業用BGM",
+      ]
+    end
+  end
+
+  describe "動画情報のパース" do
+    subject {@infos}
+    it "NicoPlayer形式のinfoテキストから動画情報をパースして、Hashで返すこと" do
+      subject.should == {
+        :nico_name => "sm6875851",
+        :nico_post => "2009/04/27 23:59:25",
+        :nico_view_counter => 6431,
+        :nico_comment_num => 340,
+        :nico_mylist_counter => 229,
+        :nico_description => "投稿者のコメント欄",
+      }
+    end
   end
 end
