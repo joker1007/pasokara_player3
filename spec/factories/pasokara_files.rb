e1137e41165d813fa644a08d8ac68aa1afbe1dcf
joker1007 <kakyoin.hierophant@gmail.com>
2011-10-16 23:23:12 +0900

    refactoring pasokara_file_spec

--- spec/factories/pasokara_files.rb
+++ spec/factories/pasokara_files.rb
@@ -3,47 +3,48 @@
 
 require "digest/md5"
 
-Factory.sequence :mp4_name do |n|
-  "test#{n}.mp4"
-end
+FactoryGirl.define do
+  sequence :mp4_name do |n|
+    "test#{n}.mp4"
+  end
 
-Factory.sequence :flv_name do |n|
-  "test#{n}.flv"
-end
+  sequence :flv_name do |n|
+    "test#{n}.flv"
+  end
 
-Factory.define :pasokara_file do |f|
-  f.name "test001.mp4"
-  f.fullpath {|p| File.join(File.expand_path(File.dirname(__FILE__)), "..", "datas", p.name)}
-  f.md5_hash {|p| Digest::MD5.hexdigest p.name }
-  f.nico_name "sm999999"
-  f.nico_post "2011-06-04 02:31:01"
-  f.nico_view_counter 1
-  f.nico_comment_num 1
-  f.nico_mylist_counter 1
-  f.duration 245
-  f.nico_description "description"
-  f.tags {["Tag1", "Tag2", "Tag3"]}
-  f.encoding false
-end
+  factory :pasokara_file do |f|
+    name "test001.mp4"
+    fullpath {|p| File.join(File.expand_path(File.dirname(__FILE__)), "..", "datas", p.name)}
+    md5_hash {|p| Digest::MD5.hexdigest p.name }
+    nico_name "sm999999"
+    nico_post "2011-06-04 02:31:01"
+    nico_view_counter 1
+    nico_comment_num 1
+    nico_mylist_counter 1
+    duration 245
+    nico_description "description"
+    tags {["Tag1", "Tag2", "Tag3"]}
+    encoding false
+  end
 
-Factory.define :pasokara_file2, :class => PasokaraFile do |f|
-  f.name "test002.mp4"
-  f.fullpath {|p| File.join(File.expand_path(File.dirname(__FILE__)), "..", "datas", "testdir1", p.name)}
-  f.md5_hash {|p| Digest::MD5.hexdigest p.name }
-end
+  factory :pasokara_file2, :class => PasokaraFile do |f|
+    name "test002.mp4"
+    fullpath {|p| File.join(File.expand_path(File.dirname(__FILE__)), "..", "datas", "testdir1", p.name)}
+    md5_hash {|p| Digest::MD5.hexdigest p.name }
+  end
 
-Factory.define :siawase_gyaku, :class => PasokaraFile do |f|
-  f.name "【ニコカラ】シアワセうさぎ（逆）(夏) OnVocal.flv"
-  f.fullpath {|p| File.join(File.expand_path(File.dirname(__FILE__)), "..", "datas", p.name)}
-  f.md5_hash {|a| Digest::MD5.hexdigest a.name }
-  f.nico_name "sm4601746"
-  f.nico_post "2011-06-04 02:31:01"
-  f.nico_view_counter 2
-  f.nico_comment_num 2
-  f.nico_mylist_counter 2
-  f.duration 2
-  f.nico_description "description"
-  f.tags {["Tag1", "Tag2", "Tag4"]}
-  f.encoding false
+  factory :siawase_gyaku, :class => PasokaraFile do |f|
+    name "【ニコカラ】シアワセうさぎ（逆）(夏) OnVocal.flv"
+    fullpath {|p| File.join(File.expand_path(File.dirname(__FILE__)), "..", "datas", p.name)}
+    md5_hash {|a| Digest::MD5.hexdigest a.name }
+    nico_name "sm4601746"
+    nico_post "2011-06-04 02:31:01"
+    nico_view_counter 2
+    nico_comment_num 2
+    nico_mylist_counter 2
+    duration 245
+    nico_description "description"
+    tags {["Tag1", "Tag2", "Tag4"]}
+    encoding false
+  end
 end
-
