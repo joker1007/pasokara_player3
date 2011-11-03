23b90db1d6c725b3fb2424c56c5f6ea784bd03ae
joker1007 <kakyoin.hierophant@gmail.com>
2011-11-03 17:20:57 +0900

    bug fix
    ログイン済ユーザの切り替えが出来ないバグを修正

--- app/controllers/custom_devise/sessions_controller.rb
+++ app/controllers/custom_devise/sessions_controller.rb
@@ -1,10 +1,17 @@
 class CustomDevise::SessionsController < Devise::SessionsController
   prepend_before_filter :require_no_authentication, :only => []
 
+  def new
+    unless session[:logined_users].nil? or session[:logined_users].empty?
+      @logined_users = User.only(:id, :nickname).find(session[:logined_users])
+    end
+    super
+  end
+
   def create
     session["warden.user.user.key"] = nil
     super
-    session[:logined_users] ||= [] 
+    session[:logined_users] ||= []
     session[:logined_users] << current_user.id unless session[:logined_users].include?(current_user.id)
   end
 
