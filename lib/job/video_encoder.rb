11cda3a50fc4e29735a63d05bd097f0d135b18c4
joker1007 <kakyoin.hierophant@gmail.com>
2011-11-10 00:02:35 +0900

    fix indent & method definition

--- lib/job/video_encoder.rb
+++ lib/job/video_encoder.rb
@@ -7,27 +7,27 @@ module Job
       pasokara.encoding = true
       pasokara.save
       case type
-      when :stream
-        stream_encode(pasokara)
-      when :safari
+      when "stream"
+        stream_encode(pasokara, host)
+      when "safari"
         safari_encode(pasokara)
       end
       pasokara.encoding = false
       pasokara.save
     end
 
-    def stream_encode(pasokara)
-        segmenter_duration = "5"
+    def self.stream_encode(pasokara, host)
+      segmenter_duration = "5"
 
-        input_file = pasokara.fullpath
-        aspect = `#{Rails.root}/lib/job/get_info.sh "#{input_file}"`.chomp
-        system("#{Rails.root}/lib/job/video_encoder.sh", input_file, "#{Rails.root}/public/video", aspect.to_s, aspect.to_s, segmenter_duration, pasokara.encode_prefix(:stream), "http://#{host}/video")
+      input_file = pasokara.fullpath
+      aspect = `#{Rails.root}/lib/job/get_info.sh "#{input_file}"`.chomp
+      system("#{Rails.root}/lib/job/video_encoder.sh", input_file, "#{Rails.root}/public/video", aspect.to_s, aspect.to_s, segmenter_duration, pasokara.encode_prefix(:stream), "http://#{host}/video")
     end
 
-    def safari_encode(pasokara)
-        input_file = pasokara.fullpath
-        aspect = `#{Rails.root}/lib/job/get_info.sh "#{input_file}"`.chomp
-        system("#{Rails.root}/lib/job/safari_encoder.sh", input_file, "#{Rails.root}/public/video", aspect.to_s, aspect.to_s, pasokara.encode_prefix(:safari))
+    def self.safari_encode(pasokara)
+      input_file = pasokara.fullpath
+      aspect = `#{Rails.root}/lib/job/get_info.sh "#{input_file}"`.chomp
+      system("#{Rails.root}/lib/job/safari_encoder.sh", input_file, "#{Rails.root}/public/video", aspect.to_s, aspect.to_s, pasokara.encode_prefix(:safari))
     end
   end
 end
