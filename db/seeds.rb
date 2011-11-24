9228f108adca5e767a6dc45c4506ed602b68434f
joker1007 <kakyoin.hierophant@gmail.com>
2011-11-25 01:18:25 +0900

    add db/seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+# coding: utf-8
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
@@ -5,3 +6,13 @@
 #
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Daley', :city => cities.first)
+
+if admin = User.where(:name => "admin").first
+  admin.password = "admin"
+  admin.password_confirmation = "admin"
+  admin.save
+  puts "adminのパスワードを初期化しました。 ID/PASS : admin/admin"
+else
+  User.create({:name => "admin", :nickname => "admin", :password => "admin", :password_confirmation => "admin"})
+  puts "初期ユーザーを作成しました。 ID/PASS : admin/admin"
+end
