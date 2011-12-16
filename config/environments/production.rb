bb79e25d9c740cf580fc0dabd1ad7a52d31ec4df
joker1007 <kakyoin.hierophant@gmail.com>
2011-12-16 14:58:25 +0900

    use nginx & unix socks

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -9,7 +9,7 @@ PasokaraPlayerRails3::Application.configure do
   config.action_controller.perform_caching = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
-  config.serve_static_assets = true
+  config.serve_static_assets = false
 
   # Compress JavaScripts and CSS
   config.assets.compress = true
@@ -24,8 +24,8 @@ PasokaraPlayerRails3::Application.configure do
   # config.assets.manifest = YOUR_PATH
 
   # Specifies the header that your server uses for sending files
-  config.action_dispatch.x_sendfile_header = "X-Sendfile" # for apache
-  # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
+  # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for apache
+  config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
