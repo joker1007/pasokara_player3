b8536d0c10f2c542718e0bd3cf5851eb874a2b5e
joker1007 <kakyoin.hierophant@gmail.com>
2012-01-15 23:46:16 +0900

    add gritter for nico_list form

--- app/controllers/nico_lists_controller.rb
+++ app/controllers/nico_lists_controller.rb
@@ -3,35 +3,35 @@ class NicoListsController < ApplicationController
   before_filter :authenticate_user!
 
   # GET /nico_lists
-  # GET /nico_lists.xml
+  # GET /nico_lists.json
   def index
     @nico_lists = NicoList.all
 
     respond_to do |format|
       format.html # index.html.erb
-      format.xml  { render :xml => @nico_lists }
+      format.json  { render :json => @nico_lists }
     end
   end
 
   # GET /nico_lists/1
-  # GET /nico_lists/1.xml
+  # GET /nico_lists/1.json
   def show
     @nico_list = NicoList.find(params[:id])
 
     respond_to do |format|
       format.html # show.html.erb
-      format.xml  { render :xml => @nico_list }
+      format.json  { render :json => @nico_list }
     end
   end
 
   # GET /nico_lists/new
-  # GET /nico_lists/new.xml
+  # GET /nico_lists/new.json
   def new
     @nico_list = NicoList.new
 
     respond_to do |format|
       format.html # new.html.erb
-      format.xml  { render :xml => @nico_list }
+      format.json  { render :json => @nico_list }
     end
   end
 
@@ -41,7 +41,7 @@ class NicoListsController < ApplicationController
   end
 
   # POST /nico_lists
-  # POST /nico_lists.xml
+  # POST /nico_lists.json
   def create
     @nico_list = NicoList.new(params[:nico_list])
 
@@ -51,18 +51,16 @@ class NicoListsController < ApplicationController
           flash[:notice] = 'ダウンロードリストが追加されました'
           redirect_to(nico_lists_path)
         }
-        format.json { render :json => @nico_list }
-        format.xml  { render :xml => @nico_list, :status => :created, :location => @nico_list }
+        format.json { render :json => @nico_list, :status => :created }
       else
         format.html { render :action => "new" }
         format.json { render :json => @nico_list.errors, :status => :unprocessable_entity  }
-        format.xml  { render :xml => @nico_list.errors, :status => :unprocessable_entity }
       end
     end
   end
 
   # PUT /nico_lists/1
-  # PUT /nico_lists/1.xml
+  # PUT /nico_lists/1.json
   def update
     @nico_list = NicoList.find(params[:id])
 
@@ -70,24 +68,22 @@ class NicoListsController < ApplicationController
       if @nico_list.update_attributes(params[:nico_list])
         format.html { redirect_to(@nico_list, :notice => 'Nico list was successfully updated.') }
         format.json { render :json => @nico_list }
-        format.xml  { head :ok }
       else
         format.html { render :action => "edit" }
         format.json { render :json => @nico_list.errors, :status => :unprocessable_entity  }
-        format.xml  { render :xml => @nico_list.errors, :status => :unprocessable_entity }
       end
     end
   end
 
   # DELETE /nico_lists/1
-  # DELETE /nico_lists/1.xml
+  # DELETE /nico_lists/1.json
   def destroy
     @nico_list = NicoList.find(params[:id])
     @nico_list.destroy
 
     respond_to do |format|
       format.html { redirect_to(nico_lists_url) }
-      format.xml  { head :ok }
+      format.json  { head :ok }
     end
   end
 end
