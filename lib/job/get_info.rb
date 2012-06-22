81385d839a3b85f68261706e2e068b317c7f5720
joker1007 <kakyoin.hierophant@gmail.com>
2012-06-22 23:50:28 +0900

    fix regexp for new ffmpeg output

--- lib/job/get_info.rb
+++ lib/job/get_info.rb
@@ -4,11 +4,11 @@ audio = Hash.new{|h,k| h[k]=""}
 while line = gets
   #p [:msg,line]
   case line
-  when /\s*Stream #\d\.\d.*?: Video: (.+?), (.+?), (.+?),/
+  when /\s*Stream #\d[:\.]\d.*?: Video: (.+?), (.+?), (.+?),/
     video['codec'] = $1
     video['format'] = $2
     video['rez'] = $3
-  when /\s*Stream #\d\.\d.*?: Audio: (.+?), (.+?), (.+?), (.+?)/
+  when /\s*Stream #\d[:\.]\d.*?: Audio: (.+?), (.+?), (.+?), (.+?)/
     audio['codec'] = $1
     audio['rate'] = $2
     audio['channels'] = $3
